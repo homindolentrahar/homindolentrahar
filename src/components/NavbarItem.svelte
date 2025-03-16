2ba40f172a33ac303b665ec817846d7d5e48ef2e
Rahardiyan Eko Widiatmoko <rahardiyanekowidiatmoko@gmail.com>
2025-03-16 07:13:01 +0700

    feat(home): Creating Hero, Works, and About section

---
+++ src/components/NavbarItem.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  export let targetId: string;
+
+  function smoothScroll(event: Event, targetId: string) {
+    event.preventDefault();
+
+    const target = document.getElementById(targetId);
+    const navbar = document.querySelector("nav");
+
+    if (!target || !navbar) return;
+
+    const navbarHeight = (navbar as HTMLElement).offsetHeight;
+
+    window.scrollTo({
+      top: target.offsetTop - navbarHeight,
+      behavior: "smooth",
+    });
+  }
+</script>
+
+<span class="p-4">
+  <a
+    href={"#" + targetId}
+    class="text-sm text-gray-500 hover:text-black hover:font-semibold transition-all duration-200"
+    on:click={(e) => smoothScroll(e, targetId)}
+  >
+    <slot />
+  </a>
+</span>
